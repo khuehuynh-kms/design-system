d6bbab5df1b432690d053ac2f3a3c0673045dfcc
Khue Huynh <khuehuynh@kms-technology.com>
2022-09-01 22:09:39 +0700

    add config

--- packages/ui-design-system/vite.config.ts
+++ packages/ui-design-system/vite.config.ts
@@ -1,10 +1,17 @@
 import vue from "@vitejs/plugin-vue";
+// import vueI18n from "@intlify/vite-plugin-vue-i18n";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // vueI18n({
+    //   include: path.resolve(__dirname, "./src/locales/**"),
+    //   runtimeOnly: false,
+    // }),
+  ],
   resolve: {
     alias: {
       "@/": new URL("./src/", import.meta.url).pathname,
