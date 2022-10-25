19771600148ed7fdfd24902832235104dcee61ce
Khue Huynh <khuehuynh@kms-technology.com>
2022-10-25 10:19:57 +0700

    uv

---
+++ packages/comon1/src/elements/InputText.vue
@@ -0,0 +1,19 @@
+<template>
+  <session>
+    <div>{{ label1 }}: <input type="text" /></div>
+    <ActionButton side="ltr"></ActionButton>
+  </session>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import ActionButton from "./ActionButton.vue";
+
+export default defineComponent({
+  props: {
+    label1: String,
+  },
+  setup(props) {
+    props.label1; // <-- type: string
+  },
+});
+</script>
